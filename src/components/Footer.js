465b8c9d36051f6081371267413c7a5e65bfe8b4
Trevor <toantu3005@gmail.com>
2024-07-22 18:54:20 +1000

    feat: Add Footer.
    style: Change flex direction on smaller screens

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer>
+            <p>This page is created by <a href="https://github.com/ToanThanhTu" target="_blank" rel="noreferrer">Thanh Toan Tu</a></p>
+            <p>22nd June 2024 | <a href="https://github.com/ToanThanhTu/markdown-previewer" target="_blank" rel="noreferrer">Source Code</a></p>
+        </footer>
+    );
+}
+
+export default Footer;
